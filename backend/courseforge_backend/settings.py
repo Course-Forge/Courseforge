b454bc642e73b25baa8de2e42bbdeb02e62d3bcb
SrikarVem <69221759+ScodingV@users.noreply.github.com>
2025-04-05 21:27:03 -0700

    docker set --> deploymentt

--- backend/courseforge_backend/settings.py
+++ backend/courseforge_backend/settings.py
@@ -26,7 +26,6 @@ YOUTUBE_API_KEY = os.environ.get("youtube_api_key")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
